cedc08f20c67a39b18fe5f81806b1bb69be704ee
Artem Tsygankov <acigankov@inbox.ru>
2020-09-25 00:04:11 +0300

    добавлен текст при конце таймера

--- timer.js
+++ timer.js
@@ -2,6 +2,7 @@ const daysEl = document.getElementById('days');
 const hoursEl = document.getElementById('hours');
 const minutesEl = document.getElementById('minutes');
 const secondsEl = document.getElementById('seconds');
+const timerEl = document.getElementById('timer');
 
 function countdown () {
     const targetDate = new Date("01.01.2021");
@@ -12,14 +13,24 @@ function countdown () {
     const minutes = Math.floor(allSeconds / 60 ) % 60;
     const seconds = Math.floor(allSeconds) % 60;
 
+    if (allSeconds <= 0) {
+        timerEl.innerHTML="Таймер закончился!";
+        return false;
+        
+    } 
+
     daysEl.innerHTML = formatTime(days);
     hoursEl.innerHTML = formatTime(hours);
     minutesEl.innerHTML = formatTime(minutes);
     secondsEl.innerHTML = formatTime(seconds);
+
+    
 }
 
 function formatTime(time) {
     return (time < 10 ? `0${time}` : time);
 }
+
+
 countdown();
 setInterval(countdown, 1000);
